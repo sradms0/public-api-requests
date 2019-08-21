5356ade1db9f6c105b4c8578790bfacfe6f3e606
sradms0 <sradms0@openmailbox.org>
2019-08-21 08:51:08 -0400

    add createDivCard function

--- js/script.js
+++ js/script.js
@@ -27,15 +27,43 @@ async function getEmployeeData() {
   return json.results;
 }
 
+/**
+ * Create div-string with employee-data
+ * @param {object} employee - The employee to be displayed
+ * @param {string} employee.name - The name of the employee
+ * @param {object} employee.location - The location of the employee
+ * @param {string} employee.email - The name of the employee
+ * @param {string} employee.picture - An array of pictures of the employee
+ * @return {string} 
+*/
+function createDivCard({name, location, email, picture}) {
+  const {first, last} = name,
+        {city, state, postcode} = location,
+        {medium} = picture;
+  return `
+    <div class="card">
+        <div class="card-img-container">
+            <img class="card-img" src="${medium}" alt="profile picture">
+        </div>
+        <div class="card-info-container">
+            <h3 id="name" class="card-name cap">${first} ${last}</h3>
+            <p class="card-text">${email}</p>
+            <p class="card-text cap">${city}, ${state}</p>
+        </div>
+    </div>
+  `;
+}
+
 /** Create employee directory*/
 function main() {
   asyncWrapper(
     (async () => {
+      const divCardContainer = document.querySelector('#gallery');
       const data = await getEmployeeData();
+      // concat new employee div-card to container
       data.forEach(i => {
-        document.body.append(JSON.stringify(i));
-
-      });
+        divCardContainer.innerHTML += createDivCard(i);
+      })
     })()
   );
 }
