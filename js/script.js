7d848dd4cfc6eaa8fa795fc6a213f6e12a398879
sradms0 <sradms0@openmailbox.org>
2019-08-22 11:26:34 -0400

    add employee modal window

--- js/script.js
+++ js/script.js
@@ -70,6 +70,46 @@ function createSearchForm() {
   </form>
   `
 }
+/**
+ * Create  modal container to display detailed employee-data
+ * @param {object} employee - The employee to be displayed
+ * @param {string} employee.name - The name of the employee
+ * @param {object} employee.location - The location of the employee
+ * @param {string} employee.email - The name of the employee
+ * @param {object} employee.picture - An array of pictures of the employee
+ * @param {string} employee.phone - The phone number of the employee
+ * @param {object} employee.dob - The employee's date of birth
+ * @return {string}
+*/
+function createModalDivContainer({name, location, email, picture, phone, dob}) {
+  const {first, last} = name,
+        {city, state, street, postcode} = location,
+        {large} = picture,
+        {date} = dob;
+
+
+  return `
+    <div class="modal-container">
+        <div class="modal">
+            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+            <div class="modal-info-container">
+                <img class="modal-img" src="${large}" alt="profile picture">
+                <h3 id="name" class="modal-name cap">${first} ${last}</h3>
+                <p class="modal-text">${email}</p>
+                <p class="modal-text cap">${city}</p>
+                <hr>
+                <p class="modal-text">${phone}</p>
+                <p class="modal-text">${street}, ${city}, ${state} ${postcode}</p>
+                <p class="modal-text">Birthday: ${new Date(date).toLocaleDateString()}</p>
+            </div>
+        </div>
+        <div class="modal-btn-container">
+            <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+            <button type="button" id="modal-next" class="modal-next btn">Next</button>
+        </div>
+    </div>
+  `;
+}
 
 /**
  * Hide all employee cards
@@ -88,6 +128,30 @@ function searchEmployees(e) {
       i.style.display = '';
     }
   })
+}
+
+/**
+ * Activate modal window of selected employee 
+ * @param {object} e - The event triggered
+*/
+function modalizeEmployee(e) {
+    const employeeDivCard = e.target.closest('.card')
+    // check if the card or any elements in the card were selected
+    if (employeeDivCard) {
+      // get employee object from 'employees' array
+      const employeeData = employees[employeeDivCard.id]
+
+      // create modal and pass all data to it
+      const modalDivContainer = document.createElement('div');
+      modalDivContainer.innerHTML = createModalDivContainer(employeeData);
+
+      // allow modal to close
+      modalDivContainer.querySelector('#modal-close-btn')
+        .addEventListener('click', e => document.body.removeChild(modalDivContainer));
+
+      // add modal to display
+      document.body.append(modalDivContainer);
+    }
 }
 
 /** Create employee directory*/
@@ -111,6 +175,9 @@ function main() {
   const searchForm = searchContainer.firstElementChild;
   searchForm.addEventListener('keyup', searchEmployees);
   searchForm.addEventListener('submit', searchEmployees);
+
+  // add modal div
+  document.body.addEventListener('click', modalizeEmployee);
 }
 
 main();
